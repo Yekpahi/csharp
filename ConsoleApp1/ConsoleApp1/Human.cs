f4b83c0196d235cca942d1d1d21ec0964e125c80
Yekpahi Prince <princeyekpahi@hotmail.fr>
2022-08-26 12:07:35 +0200

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Human.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Human
+    {
+        public string firstName;
+        public string lastName;
+
+        public Human (string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp1;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes__Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a new human
+            Human human = new Human("Bahi", "Yekpahi");
+            Console.WriteLine("{0} {1}", human.firstName, human.lastName);
+        }
+    }
+}
+
+
