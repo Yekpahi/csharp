8c8dd1018bc1eaddf7e9b79c8da890a9a62a9939
Yekpahi Prince <princeyekpahi@hotmail.fr>
2022-08-28 23:52:38 +0200

    private protected

--- myproject/Program.cs
+++ myproject/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    /*class Present {
+        public string nom;
+        public string prenom;
+
+        public void presentation () {
+            Console.WriteLine("Je me nomme {0} {1}", prenom, nom);
+        }
+    }*/
+
+    class Student {
+        protected string name;
+    }
+    internal class Program : Student
+    {
+
+        static void Main(string[] args)
+        {
+           Program p1 = new Program();
+           p1.name = "Charles";
+
+           Console.WriteLine(p1.name);
+        }
+    }
+}
