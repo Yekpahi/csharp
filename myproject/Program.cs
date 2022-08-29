04a962a1bef46a9efa2ec48ac75f9e2b38f513cf
Yekpahi Prince <princeyekpahi@hotmail.fr>
2022-08-29 12:33:26 +0200

    constructor

--- myproject/Program.cs
+++ myproject/Program.cs
@@ -1,20 +1,42 @@
 ﻿using System;
 
-namespace MyApp // Note: actual namespace depends on the project name.
-{
-   
-    internal class Program 
-    {
-      
-        static void Main(string[] args)
-        {
-          string str1 = "C# Programming ";
-            string str2 = "and C Programming";
+namespace Inheritance {
 
-            string conc = string.Concat(str1, str2);
+  // base class
+  class Animal { 
 
-            Console.WriteLine(conc);
+    public string name;
 
-        }
+    public void display() {
+      Console.WriteLine("I am an animal");
     }
+    
+  } 
+  
+  // derived class of Animal 
+  class Dog : Animal {
+    
+    public void getName() {
+      Console.WriteLine("My name is " + name);
+    }
+  }
+
+  class Program {
+
+    static void Main(string[] args) {
+
+      // object of derived class
+      Dog labrador = new Dog();
+
+      // access field and method of base class
+      labrador.name = "Rohu";
+      labrador.display();
+
+      // access method from own class
+      labrador.getName();
+
+      Console.ReadLine();
+    }
+
+  }
 }
