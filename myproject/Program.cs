d955a11cb050a092c8ca7aaa7f0cad1f4f08a7a1
Yekpahi Prince <princeyekpahi@hotmail.fr>
2022-08-29 13:56:40 +0200

    Inheritance

--- myproject/Program.cs
+++ myproject/Program.cs
@@ -2,40 +2,21 @@
 
 namespace Inheritance {
 
-  // base class
-  class Animal { 
+  abstract class Language {
 
-    public string name;
-
-    public void display() {
-      Console.WriteLine("I am an animal");
-    }
-    
-  } 
-  
-  // derived class of Animal 
-  class Dog : Animal {
-    
-    public void getName() {
-      Console.WriteLine("My name is " + name);
+    public void lang()
+    {
+        Console.WriteLine("I speak english");
     }
   }
 
-  class Program {
+  class Program : Language{
 
     static void Main(string[] args) {
 
-      // object of derived class
-      Dog labrador = new Dog();
+        Program p = new Program();
+        p.lang();
 
-      // access field and method of base class
-      labrador.name = "Rohu";
-      labrador.display();
-
-      // access method from own class
-      labrador.getName();
-
-      Console.ReadLine();
     }
 
   }
