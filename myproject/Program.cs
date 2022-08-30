8954ae90d84ecea55055f691b078a2bd1bab7b00
Yekpahi Prince <princeyekpahi@hotmail.fr>
2022-08-30 11:53:59 +0200

    Struct

--- myproject/Program.cs
+++ myproject/Program.cs
@@ -3,26 +3,26 @@ using Informatique;
 using sc = System.Console;
 
 
-namespace MyNamespace
-{
-    public class SampleClass
-    {
-        public static void myMethod()
-        {
-            Console.WriteLine("Creating my namespace");
-        }
-    }
-}
-
-
-
 namespace Inheritance
 {
+
+struct Rectangle {
+    public int largeur;
+    public int longueur;
+
+    public Rectangle (int ll, int lL) {
+        this.largeur = ll;
+        this.longueur = lL;
+    }
+}
  class Program {
  static void Main(string[] args)
     {
+        Rectangle rect = new Rectangle(60, 100);
+        Console.WriteLine("La longueur est : " + rect.longueur);
+        Console.WriteLine("La largeur est : " + rect.largeur);
 
-      MyNamespace.SampleClass.myMethod();
+      
     }
 
  }
