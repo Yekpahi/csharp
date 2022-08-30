fd71592e409dbf5a69043ba37a86992545159943
Yekpahi Prince <princeyekpahi@hotmail.fr>
2022-08-30 11:09:37 +0200

    Namespace

--- myproject/Program.cs
+++ myproject/Program.cs
@@ -2,28 +2,27 @@
 using Informatique;
 using sc = System.Console;
 
+
+namespace MyNamespace
+{
+    public class SampleClass
+    {
+        public static void myMethod()
+        {
+            Console.WriteLine("Creating my namespace");
+        }
+    }
+}
+
+
+
 namespace Inheritance
 {
  class Program {
  static void Main(string[] args)
     {
 
-        int num = 2000;
-
-        string str = "300";
-
-        Type n = num.GetType();
-
-        double a = 1.25;
-        int nulmInt = (int) a;
-
-        int b = int.Parse(str);
-
-
-        sc.WriteLine("Le type de {0} est {1}.", num, n);
-        
-        sc.WriteLine("{0} : {1}.", a, nulmInt);
-         sc.WriteLine("{0} : {1}.", str, b);
+      MyNamespace.SampleClass.myMethod();
     }
 
  }
