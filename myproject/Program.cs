498792bcdfda685d64c046fec8aec8e87d182c26
Yekpahi Prince <princeyekpahi@hotmail.fr>
2022-08-30 10:04:07 +0200

    Convert

--- myproject/Program.cs
+++ myproject/Program.cs
@@ -1,26 +1,32 @@
 ﻿using System;
 using Informatique;
+using sc = System.Console;
 
 namespace Inheritance
 {
-
-    interface Irectangle
+ class Program {
+ static void Main(string[] args)
     {
-        int aire(int a, int b);
-    }
-    class Program : Irectangle
-    {
-        public int aire(int a, int b) {
-            int aire = a * b;
-            return aire;
-        }
-        static void Main(string[] args)
-        {
-    
-            Program a1 = new Program();
-            Console.WriteLine(a1.aire(10, 20));
 
-        }
+        int num = 2000;
 
+        string str = "300";
+
+        Type n = num.GetType();
+
+        double a = 1.25;
+        int nulmInt = (int) a;
+
+        int b = int.Parse(str);
+
+
+        sc.WriteLine("Le type de {0} est {1}.", num, n);
+        
+        sc.WriteLine("{0} : {1}.", a, nulmInt);
+         sc.WriteLine("{0} : {1}.", str, b);
     }
+
+ }
+   
+
 }
