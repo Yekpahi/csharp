ca68cda60d87b7c7539756f6b852e29df586d6ef
Yekpahi Prince <princeyekpahi@hotmail.fr>
2022-09-05 11:21:45 +0200

    Checkedbox

---
+++ RadioButto/RadioButto/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RadioButto
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void radio_Checked(object sender, RoutedEventArgs e)
+        {
+            Uri resourceUri = new Uri("https://images.bfmtv.com/eh4UDIXJXc9tT3jZn443DjD6NCo=/0x0:2048x1152/2048x0/images/Barack-Obama-a-Las-Vegas-le-8-janvier-2022-1356455.jpg");
+           imcheck.Source = new BitmapImage(resourceUri);
+        }
+
+        private void radio_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Uri resourceUri = new Uri("https://resize.elle.fr/original/var/plain_site/storage/images/people/la-vie-des-people/news/michelle-obama-partage-un-tendre-message-pour-les-61-ans-de-barack-obama-4043135/97167072-1-fre-FR/Michelle-Obama-partage-un-tendre-message-pour-les-61-ans-de-Barack-Obama.jpg");
+            imcheck.Source = new BitmapImage(resourceUri);
+
+        }
+    }
+}
---
+++ checkboxes/checkboxes/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace checkboxes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cbParentCheckedChanged(object sender, RoutedEventArgs e)
+        {
+            bool newVal = (cbParent.IsChecked == true);
+            cbCheese.IsChecked = newVal;
+            cbHam.IsChecked = newVal;
+            cbTuna.IsChecked = newVal;
+        }
+
+        private void cbTopingsCheckedChanged(object sender, RoutedEventArgs e)
+        {
+            cbParent.IsChecked = null;
+            if((cbCheese.IsChecked == true) && (cbHam.IsChecked == true) && (cbTuna.IsChecked == true))
+            {
+                cbParent.IsChecked = true;
+            }
+            if((cbCheese.IsChecked == false) && (cbHam.IsChecked == false) && (cbTuna.IsChecked == false))
+            {
+                cbParent.IsChecked = false;
+            }
+        }
+
+    }
+}
